c8655a698e4205c0d0e005e9b81296b5adb86f08
Robert Amour <robertamourgoogs@gmail.com>
2018-10-28 21:34:24 -0700

    I was mistaken

--- loadcsv.py
+++ loadcsv.py
@@ -10,9 +10,10 @@ with open('wordlist.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         word = Word(text=row['Word'])
-        lang = Language.objects.get_or_create(name="english").first()
+        lang, created = Language.objects.get_or_create(name="english")
         word.save()
-        lang.save()
+        if created:
+            lang.save()
         entry = WordEntry(word=word, language=lang, ranking=row['Rank'], frequency=row['Frequency'])
         entry.save()
 
