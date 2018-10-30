68e4a805da94bfca08314a4f14564a0b188b9746
Robert Amour <robertamourgoogs@gmail.com>
2018-10-29 18:18:58 -0700

    Simplify word/language relationship

--- words/admin.py
+++ words/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Word, Language, WordEntry
+from .models import Word, Language
 
 admin.site.register(Word)
 admin.site.register(Language)
-admin.site.register(WordEntry)
--- words/models.py
+++ words/models.py
@@ -1,29 +1,8 @@
 from django.db import models
 
 
-class Word(models.Model):
-    text = models.CharField(max_length=64, unique=True, db_index=True)
-
-    def __str__(self):
-        return self.text
-
-
-class WordEntry(models.Model):
-    word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    language = models.ForeignKey('Language', on_delete=models.CASCADE)
-    ranking = models.PositiveIntegerField()
-    frequency = models.PositiveIntegerField()
-
-    def __str__(self):
-        return self.language.name + ' - ' + self.word.text
-
-    class Meta:
-        unique_together = ('language', 'ranking')
-
-
 class Language(models.Model):
     name = models.CharField(max_length=32, unique=True, db_index=True)
-    words = models.ManyToManyField(Word, through='WordEntry')
 
     def __str__(self):
         return self.name
@@ -32,3 +11,13 @@ class Language(models.Model):
         if not top_n:
             return self.words.all()
         return self.words.filter(ranking__lte=top_n)
+
+
+class Word(models.Model):
+    text = models.CharField(max_length=64, unique=True, db_index=True)
+    frequency = models.PositiveIntegerField()
+    rank = models.PositiveIntegerField(unique=True, db_index=True)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, blank=False, related_name='words')
+
+    def __str__(self):
+        return self.text
