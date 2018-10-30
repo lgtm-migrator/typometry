77af4d2a2bb5efc9d61e801a95554eba04fc871a
Robert Amour <robertamourgoogs@gmail.com>
2018-10-29 18:17:23 -0700

    Revert "I was mistaken"
    This reverts commit 30e7429f8ccf803f2cefbd568ff50e5e9cab88b1.

--- loadcsv.py
+++ loadcsv.py
@@ -10,10 +10,9 @@ with open('wordlist.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         word = Word(text=row['Word'])
-        lang, created = Language.objects.get_or_create(name="english")
+        lang = Language.objects.get_or_create(name="english").first()
         word.save()
-        if created:
-            lang.save()
+        lang.save()
         entry = WordEntry(word=word, language=lang, ranking=row['Rank'], frequency=row['Frequency'])
         entry.save()
 
