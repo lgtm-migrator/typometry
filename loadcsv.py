be01821a60fae5c17c10a0a551ddad03e2ca412b
Robert Amour <robert_amour@elcamino.edu>
2018-10-29 19:03:20 -0700

    Testing dict thing

--- loadcsv.py
+++ loadcsv.py
@@ -1,6 +1,7 @@
 import csv
 import os
 from words.models import Word, Language
+from collections import defaultdict
 
 path = "/home/main/typometry-api"
 os.chdir(path)
@@ -15,15 +16,15 @@ with open('wordlist.csv') as csvfile:
     words = []
 
     for row in reader:
-        word = Word(text=row['Word'], language=english)
-        if word not in words:
-            word.frequency = row['Frequency']
-            words.append(word)
-        else:
-            word.frequency += row['Frequency']
+        word = Word(text=row['Word'], language=english, frequency=row['Frequency'])
+        words.append(word)
 
+    freq_map = defaultdict(int)
+    for word in words:
+        freq_map[word.text] += word.frequency
+
+    print(freq_map)
     words = sorted(words, key=lambda w: w.frequency, reverse=True)
-    print(words)
     for i in range(len(words)):
         words[i].rank = i + 1
     Word.objects.bulk_create(words)
