e23423f2ebcf332539ea3e4a2931bfc9029670e0
Robert Amour <robert_amour@elcamino.edu>
2018-11-01 15:38:52 -0700

    Add WordEntry to admin panel

--- words/admin.py
+++ words/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Word, Language
+from .models import Word, Language, WordEntry
 
 admin.site.register(Word)
 admin.site.register(Language)
+admin.site.register(WordEntry)
