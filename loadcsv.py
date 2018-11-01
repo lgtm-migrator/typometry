a0108ea939d5c7bf8b62fb1959d79ddf1b05ca60
Robert Amour <robert_amour@elcamino.edu>
2018-11-01 14:18:00 -0700

    Update loadcsv script

--- loadcsv.py
+++ loadcsv.py
@@ -15,26 +15,26 @@ with open('wordlist.csv') as csvfile:
     words = {}
 
     for row in reader:
-        word = Word(text=row['Word'], language=english, frequency=int(row['Frequency']))
-        if word.text not in words:
-            words[word.text] = word
+        word = dict(text=row['Word'], language=english, frequency=int(row['Frequency']))
+        if word['text'] not in words:
+            words[word['text']] = word
         else:
-            words[word.text].frequency += int(word.frequency)
+            words[word['text']]['frequency'] += int(word['frequency'])
 
     words = words.values()
 
     for word in words:
-        print(word.text, word.frequency)
+        print(word['text'], word['frequency'])
 
-    words = sorted(words, key=lambda w: w.frequency, reverse=True)
-    raw_words = [word.text for word in words]
+    words = sorted(words, key=lambda w: w['frequency'], reverse=True)
+    raw_words = [word['text'] for word in words]
     for i in range(len(words)):
         words[i].rank = i + 1
     Word.objects.bulk_create(raw_words)
     word_entries = []
 
     for word in words:
-        word_entry = WordEntry(word=word, language=english, frequency=word.frequency, rank=word.rank)
+        word_entry = WordEntry(word=word, language=english, frequency=word['frequency'], rank=word['rank'])
         word_entries.append(word_entry)
 
     WordEntry.objects.bulk_create(word_entries)
