97ca3a8e989f7ddbf04d88d851f6fafebcc1fc3d
Robert Amour <robert_amour@elcamino.edu>
2018-11-01 21:19:25 -0700

    Better way of selecting first language

--- words/views.py
+++ words/views.py
@@ -5,7 +5,7 @@ from words.models import Language
 def word_list(request):
     if request.method == 'GET':
         # TODO: Make this language-agnostic
-        english = Language.objects.all()[0]
+        english = Language.objects.first()
         words = english.get_samples(100, 5000)
         words = [word.word.text for word in words]
         return JsonResponse(words, safe=False)
