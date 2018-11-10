b8f44d939fe026c96e624b8801e5caa7fe13c96d
Robert Amour <robert_amour@elcamino.edu>
2018-11-09 21:14:12 -0800

    Try modifying the session on word request

--- words/views.py
+++ words/views.py
@@ -5,6 +5,8 @@ from words.models import Language
 def word_list(request):
     if request.method == 'GET':
         # TODO: Make this language-agnostic
+        # modify session, test
+        request.session['test'] = 'test'
         english = Language.objects.first()
         words = english.get_samples(100, 5000)
         words = [word.word.text for word in words]
