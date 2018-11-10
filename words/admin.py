a1fc9357c92be36c3ea2525c5fd12ab4e9e4bb0b
Robert Amour <robert_amour@elcamino.edu>
2018-11-10 12:23:06 -0800

    Add new models to Admin panel

--- words/admin.py
+++ words/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Word, Language, WordEntry
+from .models import Word, Language, WordEntry, Profile, Bigram, BigramScore, WordScore
 
 admin.site.register(Word)
 admin.site.register(Language)
 admin.site.register(WordEntry)
+admin.site.register(Profile)
+admin.site.register(Bigram)
+admin.site.register(BigramScore)
+admin.site.register(WordScore)
+
