9a2d3f87eb0d5720ed3c977df59832af618a54b8
Robert Amour <robert_amour@elcamino.edu>
2018-11-10 10:20:46 -0800

    Add new serializers

--- words/serializers.py
+++ words/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from words.models import Word, Language
+from words.models import Word, Language, BigramScore, Bigram, WordScore
 
 
 class WordSerializer(serializers.ModelSerializer):
@@ -12,3 +12,21 @@ class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Language
         fields = ['name']
+
+
+class BigramSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bigram
+        fields = ['bigram']
+
+
+class BigramScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BigramScore
+        fields = ['bigram', 'user', 'count', 'average_time']
+
+
+class WordScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WordScore
+        fields = ['word', 'user', 'typos', 'average_time']
