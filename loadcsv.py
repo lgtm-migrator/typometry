369f8facf1c5c1164c254e13201bd93812dd3797
Robert Amour <robert_amour@elcamino.edu>
2018-11-17 15:17:29 -0800

    Update loadcsv.py, allow multiple languages sharing words

--- loadcsv.py
+++ loadcsv.py
@@ -8,9 +8,13 @@ os.chdir(path)
 
 with open('wordlist.csv') as csvfile:
     reader = csv.DictReader(csvfile)
+    print("Starting...")
 
-    english, new = Language.objects.get_or_create(name='english')
+    english, new = Language.objects.get_or_create(name='en_us', display_name='US English')
     if new:
+        print("Saving new language...")
+        english.name = 'en_us'
+        english.display_name = 'US English'
         english.save()
     words = {}
 
@@ -23,19 +27,29 @@ with open('wordlist.csv') as csvfile:
 
     words = words.values()
     words = sorted(words, key=lambda w: w['frequency'], reverse=True)
+    existing_words_objects = {word.text: word for word in Word.objects.all()}
+    existing_words_text = [word.text for word in existing_words_objects.values()]
     for word in words:
-        word['word'] = Word(text=word['text'])
+        if word['text'] not in existing_words_text:
+            word['word'] = Word(text=word['text'])
+            word['word'].save()
+        else:
+            word['word'] = existing_words_objects[word['text']]
 
-    raw_words = [word['word'] for word in words]
+    raw_words = [word['word'] for word in words if word['text'] not in existing_words_text]
     for i in range(len(words)):
         words[i]['rank'] = i + 1
     for word in words:
         print(word['rank'], word['text'], word['frequency'])
-    Word.objects.bulk_create(raw_words)
+
     word_entries = []
 
     for word in words:
         word_entry = WordEntry(word=word['word'], language=english, frequency=word['frequency'], rank=word['rank'])
         word_entries.append(word_entry)
 
-    WordEntry.objects.bulk_create(word_entries)
+    if new:
+        WordEntry.objects.bulk_create(word_entries)
+    else:
+        print("Updating word entries not yet implemented!")
+        print("Language not modified!")
