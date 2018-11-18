ef2d0d9ebd8e516bc7f76056cc5f8560618f2d19
Robert Amour <robert_amour@elcamino.edu>
2018-11-17 19:42:38 -0800

    Add social media auth to DRF

--- typometry/urls.py
+++ typometry/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('words/', include('words.urls')),
+    path('auth/', include('rest_auth.urls')),
+    path('auth/registration/', include('rest_auth.registration.urls'))
 ]
