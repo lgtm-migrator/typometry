d9a4bedb7265f16673236098cfcfadfdca25bbf5
Robert Amour <robertamourgoogs@gmail.com>
2019-05-07 16:23:16 -0700

    Add 'word stats' API view

--- words/urls.py
+++ words/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.word_list),
     path('smart/', views.smart_exercise),
     path('metrics/', views.RecordScores.as_view()),
+    path('stats/<str:word>/', views.GetWordStats.as_view())
 ]
