d9e34309a9f252475062c4039185eb04b780f503
Robert Amour <robertamourgoogs@gmail.com>
2019-05-07 22:37:43 -0700

    Add word stats display on word click popup

--- frontend/src/components/Word.js
+++ frontend/src/components/Word.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './Word.css'
 import { Popup } from 'semantic-ui-react'
+import axios from 'axios'
+import * as constants from './constants'
+import WordStats from './WordStats'
 
 const Word = props => (
   <span
@@ -10,13 +13,52 @@ const Word = props => (
   </span>
 )
 
-const WordMetadataPopup = props => (
-  <Popup
-    trigger={<span><Word {...props} /></span>}
-      content='Woot!'
-      on='click'
-  />
-)
+class WordMetadataPopup extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      statsObject: null,
+      loading: true
+    }
+    this.getWordStats = this.getWordStats.bind(this)
+  }
+
+  getWordStats() {
+    console.log('Requesting word stats...')
+    console.log('URL: ' + constants.WEBSITE_API_URL + '/words/stats/' + this.props.text)
+    axios.get(constants.WEBSITE_API_URL + '/words/stats/' + this.props.text)
+      .then(res => {
+        console.log('Word stats fetched')
+        this.setState( {
+          statsObject: res.data,
+          loading: false
+        })
+      })
+  }
+
+  render() {
+    const {
+      statsObject,
+      loading
+    } = this.state
+    return (
+      <Popup
+        trigger={<span><Word {...this.props} /></span>}
+        content={
+          loading ?
+            'Loading...'
+            :
+          <WordStats
+            word={this.props.text}
+            loading={loading}
+            frequency={statsObject.frequency}
+            fingering={statsObject.fingering} />
+        }
+        on='click'
+        onOpen={this.getWordStats}/>
+    )
+  }
+}
 
 const whichStyle = function (props) {
   const current = props.current
