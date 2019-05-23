b6ea8405b9ca4fc59cac7a25f0a90832cf5c135f
Robert Amour <robertamourgoogs@gmail.com>
2019-05-23 13:18:37 -0700

    Implement basic long text fetching and display

--- words/urls.py
+++ words/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.word_list),
     path('smart/', views.smart_exercise),
     path('metrics/', views.RecordScores.as_view()),
-    path('stats/<str:word>/', views.GetWordStats.as_view())
+    path('stats/<str:word>/', views.GetWordStats.as_view()),
+    path('long-text/<str:text>/<int:paragraph>/', views.GetLongText.as_view())
 ]
