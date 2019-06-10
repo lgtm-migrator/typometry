752b6bfe912a6d44a71079b46763e8225d903e34
Robert Amour <robertamourgoogs@gmail.com>
2019-06-10 16:15:39 -0700

    Add ClickAwayListener to login popper

--- frontend/src/components/LoginPopup.js
+++ frontend/src/components/LoginPopup.js
@@ -4,6 +4,8 @@ import Button from '@material-ui/core/Button'
 import Fade from '@material-ui/core/Fade'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { ClickAwayListener } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   typography: {
@@ -20,26 +22,32 @@ function LoginPopup(props) {
     setAnchorEl(anchorEl ? null : event.currentTarget)
   }
 
+  function handleClickAway() {
+    setAnchorEl(null)
+  }
+
   const open = Boolean(anchorEl)
   const id = open ? 'login-popper' : null
 
   return (
-    <div>
-      <Button variant='contained' onClick={handleClick}>
-        Log in
-      </Button>
-      <Popper id={id} open={open} anchorEl={anchorEl} transition>
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={350}>
-            <Paper>
-              <Typography className={classes.typography}>
-                Log in
-              </Typography>
-            </Paper>
-          </Fade>
-        )}
-      </Popper>
-    </div>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <div>
+        <Button variant='contained' onClick={handleClick}>
+          Log in
+        </Button>
+        <Popper id={id} open={open} anchorEl={anchorEl} transition>
+          {({ TransitionProps }) => (
+            <Fade {...TransitionProps} timeout={350}>
+              <Paper>
+                <Typography className={classes.typography}>
+                  Log in
+                </Typography>
+              </Paper>
+            </Fade>
+          )}
+        </Popper>
+      </div>
+    </ClickAwayListener>
   )
 }
 
