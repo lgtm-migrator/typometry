c1a86d68a53371847ccaa2dddaafaa9ed43e612c
Robert Amour <robertamourgoogs@gmail.com>
2019-06-10 16:09:26 -0700

    Flesh out basic login popper

--- frontend/src/components/LoginPopup.js
+++ frontend/src/components/LoginPopup.js
@@ -1,6 +1,46 @@
 import React from 'react'
+import Popper from '@material-ui/core/Popper'
+import Button from '@material-ui/core/Button'
+import Fade from '@material-ui/core/Fade'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles(theme => ({
+  typography: {
+    padding: theme.spacing(2)
+  }
+}))
 
 function LoginPopup(props) {
+  const classes = useStyles()
   const endpoint = '/login'
+  const [anchorEl,  setAnchorEl] = React.useState(null)
 
+  function handleClick(event) {
+    setAnchorEl(anchorEl ? null : event.currentTarget)
+  }
+
+  const open = Boolean(anchorEl)
+  const id = open ? 'login-popper' : null
+
+  return (
+    <div>
+      <Button variant='contained' onClick={handleClick}>
+        Log in
+      </Button>
+      <Popper id={id} open={open} anchorEl={anchorEl} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper>
+              <Typography className={classes.typography}>
+                Log in
+              </Typography>
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
+    </div>
+  )
 }
+
+export default LoginPopup
