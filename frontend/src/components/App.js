26c457223a6d21576d50eed8cca15473a1065840
Robert Amour <robertamourgoogs@gmail.com>
2019-06-11 01:57:59 -0700

    Remove pageview counting from within React

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -13,7 +13,6 @@ import * as constants from './constants'
 import ReactGA from 'react-ga'
 
 ReactGA.initialize('UA-106571309-3');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.xsrfCookieName = 'csrftoken'
